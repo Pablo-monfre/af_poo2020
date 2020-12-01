2a6ca3d62f952f728a8fe521b715128cfb82360e
Pablo_Monfre <70293915+Pablo-monfre@users.noreply.github.com>
2020-11-30 21:51:01 -0300

    reservas

---
+++ src/main/java/com/example/af_poo/model/Reservas.java
@@ -0,0 +1,17 @@
+package com.example.af_poo.model;
+
+import java.time.LocalDateTime;
+
+public class Reservas 
+{
+    private int numero;
+    private Cliente cliente;
+    private Veiculo veiculo;
+    private LocalDateTime ridataInicio;
+    private String dataFim;
+    private Float totalReserva;
+    
+    
+
+
+}
