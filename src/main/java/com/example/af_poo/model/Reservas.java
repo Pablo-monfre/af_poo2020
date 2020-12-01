fcce6092aa887f41b0b343d7784da8d0ab7468b2
Pablo_Monfre <70293915+Pablo-monfre@users.noreply.github.com>
2020-12-01 12:03:52 -0300

    atualizandoReserva

--- src/main/java/com/example/af_poo/model/Reservas.java
+++ src/main/java/com/example/af_poo/model/Reservas.java
@@ -7,11 +7,60 @@ public class Reservas
     private int numero;
     private Cliente cliente;
     private Veiculo veiculo;
-    private LocalDateTime ridataInicio;
-    private String dataFim;
-    private Float totalReserva;
+    private LocalDateTime dataInicio;
+    private boolean dataFim;
+   // private double totalReserva;
     
-    
+    public int getNumero() 
+    {
+        return numero;
+    }
 
+    public void setNumero(int numero) 
+    {
+        this.numero = numero;
+    }
+
+    public Cliente getCliente()
+    {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) 
+    {
+        this.cliente = cliente;
+    }
+
+    public Veiculo getVeiculo()
+    {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) 
+    {
+        this.veiculo = veiculo;
+    }
+
+    public LocalDateTime getDataInicio() 
+    {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDateTime dataInicio) 
+    {
+        this.dataInicio = dataInicio;
+    }
+
+    public boolean isDataFim() 
+    {
+        return dataFim;
+    }
+
+    public void setDataFim(boolean dataFim) 
+    {
+        this.dataFim = dataFim;
+    }
+
+    
 
 }
